f74cd343c7d03564cac8488303b4324de85ae022
Karuna Grewal <karunagrewal98@gmail.com>
2019-09-15 15:38:10 +0530

    add VGA text ui

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+use core::panic::PanicInfo;
+//https://doc.rust-lang.org/nomicon/panic-handler.html
+// being a no_std application it doesn't have linkage to std funcs.
+//implement panic handler for panic! macro
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+//avoid name mangling in case of the `_start` function name
+#[no_mangle]
+//foreign function interface; use C style calling convention
+pub extern "C" fn _start() -> ! {
+
+    //https://wiki.osdev.org/Text_UI
+    let greeting = b"Text UI";
+    //let vga_buffer_ptr: &mut [u8];
+    unsafe {
+        let vga_buffer_ptr = &mut (*(0xB8000 as *mut &mut[u8]));
+
+        for (index, byte) in greeting.iter().enumerate() { 
+            vga_buffer_ptr[index*2] = *byte;
+            //foreground color
+            vga_buffer_ptr[index*2 + 1] = 0x0e;
+        }
+    }
+    loop {}
+}
